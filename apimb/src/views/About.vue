c7677bc8c1f03f1ce41a58ff91b902021a4b03b8
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-11-12 10:31:51 -0500

    7654

--- apimb/src/views/About.vue
+++ apimb/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <img alt="1984 meme" src="/assets/1984LG.png">
+    <img alt="1984 meme" src="../assets/1984LG.png">
   </div>
 </template>
