a0948a5555d688c9d0ba0c83477559b99a60ac7f
AlexanderKokhno <71799901+AlexanderKokhno@users.noreply.github.com>
2021-11-23 12:29:57 -0500

    QAW

---
+++ apimb/src/components/SearchComp.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="searchBody">
+    <h1>{{ msg }}</h1>
+    <h2></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchComp',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      apiKey: 10,
+      apiSearch: 'http://store.steampowered.com/api/appdetails?appids=' + this.apiKey
+    }
+  },
+  mounted() {
+    fetch(this.apiSearch, {
+      method: 'GET',
+      headers: {
+        // appids_key: this.apiKey
+      }
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      console.log(res);
+    }) 
+  }
+}
+</script>
+<style >
+.searchBody{
+  background-color: lightskyblue;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
--- apimb/src/views/Home.vue
+++ apimb/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <HeaderComp msg="Main page (Search All)"/>
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchComp msg="Crap is operational and most likely broken"/>
     <FooterComp msg="FooterComp read"/>
   </div>
 </template>
@@ -10,7 +10,7 @@
 <script>
 // @ is an alias to /src
 import HeaderComp from '@/components/HeaderComp.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchComp from '@/components/SearchComp.vue'
 import FooterComp from '@/components/FooterComp.vue'
 // import FooterComp from '@/components/FooterComp.vue'
 
@@ -18,7 +18,7 @@ export default {
   name: 'Home',
   components: {
     HeaderComp,
-    HelloWorld,
+    SearchComp,
     FooterComp
   }
 }
